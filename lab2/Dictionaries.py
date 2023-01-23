ec46eca8d350d447b58e4a17db330dc81ea42280
Nk123-code <nicekiil81@gmail.com>
2023-01-23 17:32:15 +0600

    intal commit

---
+++ lab2/Dictionaries.py
@@ -0,0 +1,45 @@
+# Exaercise 1
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+# Exaercise 2
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+
+
+# Exaercise 3
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+
+# Exaercise 4
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+# Exaercise 5
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
