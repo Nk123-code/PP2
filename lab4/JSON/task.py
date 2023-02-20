a4e13e1271b4142383d22f7e1273f368aab7ae46
Nk123-code <nicekiil81@gmail.com>
2023-02-20 17:51:16 +0600

    Update task.py

--- lab4/JSON/task.py
+++ lab4/JSON/task.py
@@ -1,15 +1,18 @@
 import json
-with open("data.json" , "r") as read_file:
+
+with open("data.json", "r") as read_file:
     data = json.load(read_file)
 
-print(data)
-print("""Interface Status
-================================================================================""")
-print("""DN                                             Description          Speed                      MTU """)
-for i, k in data["imdata"][0]['l1PhysIf']["attributes"].items():
-    if i == 'dn':
-        print(k, end="                          ")
-    if i == "speed":
-        print(k, end="                                         ")
-    if i == "mtu":
-        print(k, end="                   ")
+print("Interface Status")
+print("=" * 80)
+print("{:<50} {:<20} {:<10} {:<10}".format("DN", "Description", "Speed", "MTU"))
+print("------------------------------------------------- --------------------  ------   ------")
+
+for x in range(3):
+    attributes = data["imdata"][x]["l1PhysIf"]["attributes"]
+    dn = attributes.get("dn", "")
+    speed = attributes.get("speed", "inherit")
+    mtu = attributes.get("mtu", "")
+    print("{:<71} {:<10} {:<10}".format(dn, speed, mtu))
+
+# I used ChatGPT to write it , but I understood it
