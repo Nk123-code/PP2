8ad4a33aa43ab1ec99de201e6ad300102ae2845c
Nk123-code <nicekiil81@gmail.com>
2023-02-20 17:55:06 +0600

    Update task.py

--- lab4/JSON/task.py
+++ lab4/JSON/task.py
@@ -10,9 +10,7 @@ print("------------------------------------------------- --------------------  -
 
 for x in range(3):
     attributes = data["imdata"][x]["l1PhysIf"]["attributes"]
-    dn = attributes.get("dn", "")
-    speed = attributes.get("speed", "inherit")
-    mtu = attributes.get("mtu", "")
-    print("{:<71} {:<10} {:<10}".format(dn, speed, mtu))
-
-# I used ChatGPT to write it , but I understood it
+    dn = attributes.get("dn", "Not found!")
+    speed = attributes.get("speed", "Not found!")
+    mtu = attributes.get("mtu", "Not found!")
+    print("{:<71} {:<10} {:<10}".format(dn, speed, mtu)) # {:<int} пробели после элемента
